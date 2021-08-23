a39589f00756363a4d204f52fba28545680afe7b
Owen Taylor <48262631+owentechnologist@users.noreply.github.com>
2021-08-23 12:12:08 -0500

    Initial commit

---
+++ src/main/java/com/redislabs/sa/ot/Main.java
@@ -0,0 +1,90 @@
+package com.redislabs.sa.ot;
+
+import com.redislabs.sa.ot.util.JedisConnectionFactory;
+import io.rebloom.client.Client;
+import org.apache.commons.lang3.NotImplementedException;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class Main {
+
+    //static JedisPool jedisPool = JedisConnectionFactory.getInstance().getJedisPool();
+
+
+    public static void main(String[] args){
+        System.out.println("testing");
+        loadSortedSetData(400);
+        System.out.println("testing2");
+        //loadTopKData(1000);
+        //loadTSData(1000);
+    }
+
+    public static void loadSortedSetData(int howMany){
+        for(int x=0;x<howMany;x++){
+            System.out.println("loadSortedSetData: Starting Thread..."+x);
+            AThreadClass aThreadClass = new AThreadClass();
+            aThreadClass.setTaskName("sortedSet");
+            aThreadClass.setTaskNumber(1000);
+            try{
+                // does this sleep fix the connection issue?
+                Thread.sleep(200);
+                Thread thread = new Thread(aThreadClass);
+                thread.start();
+            }catch(Throwable t){
+                t.printStackTrace();
+            }
+        }
+    }
+
+    public static void loadTopKData(int howMany){
+        for(int x=1;x==howMany;x++){
+            System.out.println("loadTopKData: Starting Thread..."+x);
+            AThreadClass aThreadClass = new AThreadClass();
+            aThreadClass.setTaskName("topK");
+            aThreadClass.setTaskNumber(5000);
+            try{
+                Thread thread = new Thread(aThreadClass);
+                thread.start();
+            }catch(Throwable t){
+                t.printStackTrace();
+            }
+        }
+    }
+}
+
+class AThreadClass implements Runnable{
+    static final JedisPool jedisPool = JedisConnectionFactory.getInstance().getJedisPool();
+
+    String taskName;
+    int taskNumber=0;
+
+    public void setTaskName(String taskName){
+        System.out.println("taskName = "+taskName);
+        this.taskName = taskName;
+    }
+    public void setTaskNumber(int taskNumber){
+        System.out.println("taskNumber = "+taskNumber);
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void run() {
+        if(taskName.equalsIgnoreCase("sortedSet")){
+            try(Jedis jedis = jedisPool.getResource()){
+                for(int x=0;x<taskNumber;x++){
+                    jedis.zadd("z:voters",x,(100-(x%23)+"vote"));
+                }
+            }
+        }else if(taskName.equalsIgnoreCase("topK")){
+            try(io.rebloom.client.Client topk = new Client(jedisPool)){
+                topk.topkCreateFilter("top10voters",10,2000,10,.001);
+                for(int x=0;x<taskNumber;x++){
+                    topk.topkAdd("top10voters",(100-(x%23)+"vote"));
+                }
+            }
+        }else{
+            throw new NotImplementedException("sorry, no way to do that Hal...");
+        }
+
+    }
+}
---
+++ src/main/java/com/redislabs/sa/ot/util/JedisConnectionFactory.java
@@ -0,0 +1,125 @@
+package com.redislabs.sa.ot.util;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Properties;
+
+public class JedisConnectionFactory {
+    public static final String hostPropertyName = "REDIS_HOST";
+    public static final String portPropertyName = "REDIS_PORT";
+    public static final String userPropertyName = "REDIS_USER";
+    public static final String passwordPropertyName = "REDIS_PASSWORD";
+    public static final String timeoutPropertyName = "REDIS_TIME_OUT";
+    public static final String maxIdlePropertyName = "REDIS_MAX_IDLE";
+    public static final String minIdlePropertyName = "REDIS_MIN_IDLE";
+    public static final String maxTotalPropertyName = "REDIS_MAX_TOTAL";
+    public static final String maxWaitPropertyName = "REDIS_MAX_WAIT";
+
+    private static JedisPool jedisPool;
+    private static Properties config = null;
+    private static JedisConnectionFactory instance = null;
+    private HostAndPort hostAndPort;
+
+    public JedisConnectionFactory() {
+        config = PropertyFileFetcher.loadProps("jedisconnectionfactory.properties");
+        if(null == config){
+            System.out.println("\t[JedisConnectionFactory] USING HARD-CODED CONFIGURATION ... properties files not loaded...");
+            config = new Properties();
+            config.put(hostPropertyName,"localhost");
+            config.put(portPropertyName,"6379");
+            config.put(userPropertyName,"default");
+            config.put(passwordPropertyName,"");
+            config.put(timeoutPropertyName,"15000");
+            config.put(maxIdlePropertyName,"8");
+            config.put(minIdlePropertyName,"2");
+            config.put(maxTotalPropertyName,"8");
+            config.put(maxWaitPropertyName,"5000");
+        }
+        String host = (String) config.get(hostPropertyName);
+        String port = (String) config.get(portPropertyName);
+        hostAndPort = new HostAndPort(host,Integer.parseInt(port) );
+        initPool(hostAndPort);
+    }
+
+    private JedisPoolConfig initPoolConfig(){
+        JedisPoolConfig poolConf = new JedisPoolConfig();
+        poolConf.setMaxTotal(Integer.parseInt(config.getProperty(maxTotalPropertyName)));
+        /* maximum active connections
+            Redis Enterprise can handle significantly more connections so make this number high
+            If using threads 2-3x the thread count is probably a safe rule of thumb
+            be sure to return connections to the pool
+        */
+        poolConf.setMaxIdle(Integer.parseInt(config.getProperty(maxIdlePropertyName)));
+        /* The maximum number of connections that should be kept in the idle pool if isPoolSweeperEnabled() returns false.
+           Connections start getting closed here if idle if you have long running idle connections consider matching setMaxTotal
+        */
+        poolConf.setMinIdle(Integer.parseInt(config.getProperty(minIdlePropertyName)));
+        /* The minimum number of established connections that should be kept in the pool at all times.
+           If using threads 1.25-1.5x the number of threads is safe
+           This will ensure that connections are kept to the back end so they will recycle quickly
+        */
+        poolConf.setTestOnBorrow(false);
+        /* when true - send a ping before when we attempt to grab a connection from the pool
+           Generally not recommended as while the PING command (https://redis.io/commands/PING) is relatively lightweight
+           if there is much borrowing happening this can increase traffic if the number of operations per connection is low
+        */
+        poolConf.setTestOnReturn(false);
+        /* when true - send a ping upon returning a pool connection
+           I cannot imagine a scenario where this would be useful
+        */
+        poolConf.setTestWhileIdle(true);
+        /* when true - send ping from idle resources in the pool
+           Again the ping is not expensive
+           Recommend setting this to true if you have a firewall between client and server that disconnects idle TCP connections
+           Also common issue on the cloud with load balancers (https://aws.amazon.com/blogs/aws/elb-idle-timeout-control/)
+        */
+        poolConf.setMaxWaitMillis(Integer.parseInt(config.getProperty(maxWaitPropertyName)));
+        /* set max timeout in milliseconds for write operations
+           default is -1 which means wait forever
+           Tune this carefully - often a good idea to slightly exceed your redis SLOWLOG settings,
+           so you can view what is taking so long (https://redis.io/commands/slowlog)
+        */
+        return poolConf;
+    }
+
+    private void initPool(HostAndPort hostAndPort) {
+        String user = null;
+        String password = null;
+        if(!(null == config.get(passwordPropertyName))) {
+            password = config.get(passwordPropertyName).toString();
+        }
+        if ((null == password) || (password.isEmpty())) {
+            user = null;
+            password = null;
+        } else {
+            user = config.getProperty(userPropertyName);
+            if (user.isEmpty()) {
+                user = null;
+            }
+        }
+
+        String timeoutStr = config.get(timeoutPropertyName).toString();
+        int timeout;
+        if (!timeoutStr.isEmpty()) {
+            timeout = Integer.parseInt(timeoutStr);
+        } else {
+            timeout = redis.clients.jedis.Protocol.DEFAULT_TIMEOUT;
+        }
+
+        jedisPool = new JedisPool(initPoolConfig(), hostAndPort.getHost(), hostAndPort.getPort(), timeout, user, password);
+    }
+
+    public JedisPool getJedisPool(){
+        return jedisPool;
+    }
+
+    public static JedisConnectionFactory getInstance() {
+        if (instance == null) {
+            instance = new JedisConnectionFactory();
+        }
+        return instance;
+    }
+
+}
